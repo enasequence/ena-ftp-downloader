05e13cc3b0f7999c8c5bddf489714fe54633abe4
suranj <suranj@ebi.ac.uk>
2017-07-14 16:57:47 +0100

    portal search interface added. searches merged into one for warehouse search.

--- src/main/java/uk/ac/ebi/ena/ftp/service/WarehouseQuery.java
+++ src/main/java/uk/ac/ebi/ena/ftp/service/WarehouseQuery.java
@@ -9,7 +9,9 @@ import uk.ac.ebi.ena.ftp.model.RemoteFile;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by suranj on 27/05/2016.
@@ -18,13 +20,24 @@ public class WarehouseQuery {
     public static final String ERA_ANALYSIS_ID_PATTERN = "[ESDR]RZ[0-9]+";
     private final static Logger log = LoggerFactory.getLogger(WarehouseQuery.class);
 
-    public List<RemoteFile> query(String accession, String type) {
+    public Map<String, List<RemoteFile>> query(String accession) {
         // URL stump for programmatic query of files
         String resultDomain = getResultDomain(accession);
-        if (resultDomain.equals("analysis") && type.equals("fastq")) {
-            return new ArrayList<>();
+        String[] types = {};
+        if (resultDomain.equals("analysis")) {
+            types = new String[]{"submitted"};
+        } else {
+            types = new String[]{"fastq", "submitted", "sra"};
         }
-        String url = "http://www.ebi.ac.uk/ena/data/warehouse/filereport?accession=" + accession + "&result=" + resultDomain + "&fields=" + type + "_ftp," + type + "_bytes," + type + "_md5";
+        String fields = "";
+        for (int t = 0; t < types.length; t++) {
+            String type = types[t];
+                        fields += type + "_ftp," + type + "_bytes," + type + "_md5";
+            if (t < types.length - 1) {
+                fields += ",";
+            }
+        }
+        String url = "http://www.ebi.ac.uk/ena/data/warehouse/filereport?accession=" + accession + "&result=" + resultDomain + "&fields=" + fields;
         try {
             // Build URL, Connect and get results reader
             List<String> fileStrings = null;
@@ -33,16 +46,50 @@ public class WarehouseQuery {
             fileStrings = IOUtils.readLines(yc.getInputStream());
             yc.getInputStream().close();
             if (fileStrings.size() > 1) {
-                return parseFileReport(fileStrings);
+                return parseFileReport(fileStrings, types, 0);
             }
-            return new ArrayList<>();
+            return new HashMap<>();
 
         } catch (Exception e) {
             log.warn("Error with warehouse query", e);
         }
-        return new ArrayList<>();
+        return new HashMap<>();
     }
 
+    public Map<String, List<RemoteFile>> portalQuery(String resultDomain, String query) {
+        // URL stump for programmatic query of files
+        String[] types = {};
+        if (resultDomain.equals("analysis")) {
+            types = new String[]{"submitted"};
+        } else {
+            types = new String[]{"fastq", "submitted", "sra"};
+        }
+        String fields = "";
+        for (int t = 0; t < types.length; t++) {
+            String type = types[t];
+            fields += type + "_ftp," + type + "_bytes," + type + "_md5";
+            if (t < types.length - 1) {
+                fields += ",";
+            }
+        }
+        String url = "https://www.ebi.ac.uk/ena/portal/api/search?query=" + query + "&result=" + resultDomain + "&fields=" + fields;
+        try {
+            // Build URL, Connect and get results reader
+            List<String> fileStrings = null;
+            URL enaQuery = new URL(url);
+            URLConnection yc = enaQuery.openConnection();
+            fileStrings = IOUtils.readLines(yc.getInputStream());
+            yc.getInputStream().close();
+            if (fileStrings.size() > 1) {
+                return parseFileReport(fileStrings, types, 1);
+            }
+            return new HashMap<>();
+
+        } catch (Exception e) {
+            log.warn("Error with warehouse query", e);
+        }
+        return new HashMap<>();
+    }
     private String getResultDomain(String accession) {
         if (accession.matches(ERA_ANALYSIS_ID_PATTERN)) {
             return "analysis";
@@ -50,27 +97,44 @@ public class WarehouseQuery {
         return "read_run";
     }
 
-    private List<RemoteFile> parseFileReport(List<String> fileStrings) {
-        List<RemoteFile> files = new ArrayList<>();
-        for (int f = 1; f < fileStrings.size(); f++) {// skip header line
-            if (StringUtils.isNotBlank(fileStrings.get(f))) {
-                String[] parts = fileStrings.get(f).split("\\s");
-                if (StringUtils.contains(parts[0], ";")) {
-                    String[] fileParts = parts[0].split(";");
-                    String[] sizeParts = parts[1].split(";");
-                    String[] md5Parts = parts[2].split(";");
-                    for (int p = 0; p < fileParts.length; p++) {
-                        RemoteFile file = new RemoteFile(StringUtils.substringAfterLast(fileParts[p], "/"), Long.parseLong(sizeParts[p]), fileParts[p], md5Parts[p]);
-                        files.add(file);
+    private Map<String, List<RemoteFile>> parseFileReport(List<String> fileStrings, String[] types, int skipFields) {
+        Map<String, List<RemoteFile>> map = new HashMap<>();
+        try {
 
+            for (String type : types) {
+                map.put(type, new ArrayList<>());
+            }
+            for (int f = 1; f < fileStrings.size(); f++) {// skip header line
+                if (StringUtils.isNotBlank(fileStrings.get(f))) {
+                    String[] parts = fileStrings.get(f).split("\\s");
+                    int typeIndex = skipFields;
+                    for (String type : types) {
+                        if (parts.length > typeIndex) {
+                            List<RemoteFile> files = map.get(type);
+                            if (StringUtils.isBlank(parts[0 + typeIndex])) {
+                                continue;
+                            }
+                            if (StringUtils.contains(parts[0 + typeIndex], ";")) {
+                                String[] fileParts = parts[0 + typeIndex].split(";");
+                                String[] sizeParts = parts[1 + typeIndex].split(";");
+                                String[] md5Parts = parts[2 + typeIndex].split(";");
+                                for (int p = 0; p < fileParts.length; p++) {
+                                    RemoteFile file = new RemoteFile(StringUtils.substringAfterLast(fileParts[p], "/"), Long.parseLong(sizeParts[p]), fileParts[p], md5Parts[p]);
+                                    files.add(file);
+                                }
+                            } else {
+                                RemoteFile file = new RemoteFile(StringUtils.substringAfterLast(parts[0 + typeIndex], "/"), Long.parseLong(parts[1 + typeIndex]), parts[0 + typeIndex], parts[2 + typeIndex]);
+                                files.add(file);
+                            }
+                        }
+                        typeIndex += 3;
                     }
-
-                } else {
-                    RemoteFile file = new RemoteFile(StringUtils.substringAfterLast(parts[0], "/"), Long.parseLong(parts[1]), parts[0], parts[2]);
-                    files.add(file);
                 }
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return files;
+        return map;
     }
+
 }
